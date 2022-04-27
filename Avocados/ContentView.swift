a96fcff1ddbf8ed878202f872af695b1defb2a6e
NT <usedforfunny@gmail.com>
2022-04-27 17:26:57 +0200

    Added some new assets

--- Avocados/ContentView.swift
+++ Avocados/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
