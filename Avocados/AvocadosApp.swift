461bc82ebf6047463ce423d20ff9879f17bbba6c
NT <usedforfunny@gmail.com>
2022-04-27 16:44:49 +0200

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Alexandre Talatinian on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
